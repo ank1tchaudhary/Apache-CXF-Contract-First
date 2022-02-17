ca2686a526bfb0f32ad19ae53b64ba5ea32076cd
Ankit Chaudhary <ankit.chaudhary@tietoevry.com>
2022-02-17 09:41:28 +0530

    Updated

---
+++ demo-app/src/test/java/te/cxf/demoapp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package te.cxf.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
